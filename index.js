7fc744780df34a32929879346e53f9e771dd31b2
Desk-19 <ateeb@shuraa.in>
2024-06-20 11:50:57 +0530

    uploaded

---
+++ index.js
@@ -0,0 +1,50 @@
+import express from "express";
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import path from "path";
+import fs from "fs";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const app = express();
+
+// Parsers For Form
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public'))); // Setting Up Public Static Files By Creating Public Folder In Root
+app.use(express.urlencoded({extended: true}));
+
+//Ejs Setup (Create views folder in root)
+app.set('view engine', 'ejs');
+
+app.get("/", function(req, res){
+    fs.readdir(`./files`,function(err,files){
+        res.render("index", {files :files});
+    })
+    
+});
+
+app.get("/file/:filename", function(req, res){
+    fs.readFile(`./files/${req.params.filename}`,"utf-8" ,function(err,filedata){
+        res.render("show", {filename:req.params.filename, filedata:filedata})
+    })   
+});
+
+app.get("/edit/:filename", function(req, res){
+    res.render("edit", {filename:req.params.filename}) 
+});
+
+app.post("/edit", function(req, res){
+    fs.rename(`./files/${req.body.previous}`, `./files/${req.body.new}`, function(err){
+        res.redirect("/");
+    }) 
+});
+
+app.post("/create", function(req, res){
+    fs.writeFile(`./files/${req.body.title.split(" ").join("")}.txt`, req.body.details, function(err,){
+        res.redirect("/")
+    })
+});
+
+
+app.listen(3000);
